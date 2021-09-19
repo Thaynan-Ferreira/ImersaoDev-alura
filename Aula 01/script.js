b3e67391dbf1a8eccd32598ed1785c9fc8c9b209
Thaynan Ferreira <thaynan_ferreira@outlook.com.br>
2021-09-19 13:44:23 -0300

    script em console

--- Aula 01/script.js
+++ Aula 01/script.js
@@ -1,7 +1,14 @@
- var nota1 = 9.5
+ var nota1 = 7.5
  var nota2 = 7.8
  var nota3 = 4.3
  var nota4 = 2.7
 
  var mediaFinal = (nota1 + nota2 + nota3 + nota4) / 4
  console.log(mediaFinal.toFixed(1))
+
+ if(mediaFinal > 6){
+     console.log(`APROVADO, sua nota final é ${mediaFinal}`)
+ }
+ else{
+     console.log(`REPROVADO, sua nota final É ${mediaFinal}`)
+ }
