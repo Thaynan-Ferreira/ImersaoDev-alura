1bf6d2412312a78a44abe7941ac29b8b4168e197
Thaynan Ferreira <thaynan_ferreira@outlook.com.br>
2021-09-26 10:36:19 -0300

    conversor de moedas

--- Aula 02/ex002.js
+++ Aula 02/ex002.js
@@ -1,4 +1,17 @@
+var res = document.querySelector("h2#valorConvertido")
+var resBTC = document.querySelector("h2#valorBitcoin")
+var valor = document.querySelector("input#valor")
+
 function converter(){
-    var valor = document.querySelector("input#valor")
-    console.log(valor.value)
+    var valorEmReal = valor.value * 5.34
+    res.innerHTML = `O valor U$${valor.value} é R$${valorEmReal}`
+    var valorBitcoin = valor.value * 0.000023
+    resBTC.innerHTML = `${valorBitcoin}BTC`
+}
+
+function converterEuro(){
+    var valorEmEuro = valor.value * 0.85
+    res.innerHTML = `O valor U$${valor.value} é igual á €${valorEmEuro}`
+    var valorBitcoin = valor.value * 0.000023
+    resBTC.innerHTML = `${valorBitcoin}BTC`
 }
