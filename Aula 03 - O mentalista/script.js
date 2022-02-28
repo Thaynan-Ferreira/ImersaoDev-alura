9eeb5f081171b978c448326bcada3fd1fc5e2986
Thaynan Ferreira <thaynan_ferreira@outlook.com.br>
2022-02-28 12:33:26 -0300

    criacaoScript

---
+++ Aula 03 - O mentalista/script.js
@@ -0,0 +1,17 @@
+var res = (document.querySelector("h2#resposta"))
+var numSecreto = 5
+
+function chutar(){
+    var chute = parseInt(document.querySelector("input#valor").value)
+    if (chute == numSecreto) {
+        res.innerHTML = `Parabens o valor secreto é ${numSecreto}`
+    }
+
+    else if (chute > 10 || chute < 0) {
+        res.innerHTML = `o numero ${chute} não está entre 0 e 10`
+    }
+
+    else {
+        res.innerHTML = `você errou!` 
+    }
+}
