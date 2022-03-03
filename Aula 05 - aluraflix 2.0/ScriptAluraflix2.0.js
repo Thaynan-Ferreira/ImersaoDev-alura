598a0f64f228ee88c34e2a8551966bbf120a5b3b
Thaynan Ferreira <thaynan_ferreira@outlook.com.br>
2022-03-02 21:29:15 -0300

    Finalização da 5° aula sem os desafios

--- Aula 05 - aluraflix 2.0/ScriptAluraflix2.0.js
+++ Aula 05 - aluraflix 2.0/ScriptAluraflix2.0.js
@@ -2,12 +2,16 @@ function addFilme(){
     var campoFilme = document.querySelector("input#link").value
 
     if(campoFilme.endsWith(".jpg")||campoFilme.endsWith(".png")){
-        var elemento = "<img src=" + campoFilme + ">"
-        var listaFilmes = document.querySelector("section#listaFilmes")
-        listaFilmes.innerHTML = listaFilmes.innerHTML + elemento
+        listarFilmesNaTela(campoFilme) //passagem de parametro
     }
     else{
         alert("Endereço invalido, formatos aceitos: .jpg e .png")
     }
     document.querySelector("input#link").value = ""
+}
+
+function listarFilmesNaTela(filme){ //recebmento de parametro pode ser diferente da variavel original
+    var elemento = "<img src=" + filme + ">"
+        var listaFilmes = document.querySelector("section#listaFilmes")
+        listaFilmes.innerHTML = listaFilmes.innerHTML + elemento
 }
