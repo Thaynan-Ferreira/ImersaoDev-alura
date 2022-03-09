af4f91e4b4b6d4a9750f4cc58ffce0ba017a7459
Thaynan Ferreira <thaynan_ferreira@outlook.com.br>
2022-03-09 06:51:21 -0300

    Primeira parte da aula 7

---
+++ Aula 07 - Super Trunfo/scriptSuperTrunfo.js
@@ -0,0 +1,26 @@
+let cartas = [
+    {
+        nome: "Bulbasauro",
+        atributos: {
+            ataque: 7,
+            defesa: 8,
+            magia: 6
+        }
+    },
+    {
+        nome: "Pikachu",
+        atributos: {
+            ataque: 8,
+            defesa: 5,
+            magia: 8
+        }
+    },
+    {
+        nome: "Dragonite",
+        atributos: {
+            ataque: 9,
+            defesa: 7,
+            magia: 7
+        }
+    }
+]
